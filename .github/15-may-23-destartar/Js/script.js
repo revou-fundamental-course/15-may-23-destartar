ae48e848fd05ea1606d365fcdc8def0cd7de142e
destartar <133770103+destartar@users.noreply.github.com>
2023-05-25 18:36:05 +0700

    Add files via upload

---
+++ .github/15-may-23-destartar/Js/script.js
@@ -0,0 +1,7 @@
+function hitungLuas(){
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    document.getElementById("output-luas").innerHTML = '<span>'+sisiLuas+'</span>';
+    console.log(sisiLuas);
+}
+
